68c5570999b3533dc29c28f1cb5a11df1eaa38da
brothertona <brothermanandrew@gmail.com>
2015-06-11 00:20:56 -0400

    Second commit- added DatePickerFragment, added Date Dialog, added dialog_date.xml for DatePicker

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -3,11 +3,14 @@ package com.bignerdranch.android.criminalintent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.NavUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -50,6 +53,7 @@ public class CrimeFragment extends android.support.v4.app.Fragment {
         UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
         mCrime = new Crime();
+        setHasOptionsMenu(true);
     }
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -61,10 +65,27 @@ public class CrimeFragment extends android.support.v4.app.Fragment {
         }
     }
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+             if(NavUtils.getParentActivityName(getActivity()) != null){
+                 NavUtils.navigateUpFromSameTask(getActivity());
+             }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
-
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+            if(NavUtils.getParentActivityName(getActivity()) != null){
+                getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+            }
+        }
         mTitleField = (EditText) v.findViewById(R.id.crime_title);
         mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
